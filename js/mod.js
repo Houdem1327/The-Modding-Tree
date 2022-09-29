cb4332e214e6c17d2de355caee203ab36bef77f1
Houdem <Houdem1327@users.noreply.github.com>
2022-09-29 15:16:41 +0200

    Update mod.js
    The beginning

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Village",
+	id: "village",
+	author: "Houdem",
+	pointsName: "citizens",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
